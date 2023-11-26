4e17cae414e2a1fb6c85ca6fcd04649dd124ccd7
Liang <miscoder001@gmail.com>
2023-11-26 16:29:41 +0800

    自行輸入urlEntry下載網頁

--- GUIDemo1.py
+++ GUIDemo1.py
@@ -8,24 +8,35 @@ from tkinter import *
 
 #
 def URLGetClick():  # 下載網頁
-    response = requests.get(url="https://tw.stock.yahoo.com/")
+    # 取得文字框Entry內的URL
+    urlStr = urlEntry.get()
+    response = requests.get(url=urlStr)
+    # lab2 的內容顯示為 urlStr 下載成功
     print(response.text)
+
+def clearURLEntry():
+    urlEntry.delete(0,5)
 #
 
 win = Tk()  # 建立視窗物件
 win.title("這是視窗版爬蟲範例")
 win.geometry("800x600") # 呼叫 geometry 設定視窗大小
+
 # 加入標籤
 lab1 = Label(win, text="網址:" , font=('Arial',15), bg="yellow", fg="red")   #第一個加入視窗 win 的物件
 lab1.pack()
+#加入 網址文字框輸入
+urlEntry = Entry(win, font=('Arial',15), fg='blue',width=40)
+urlEntry.pack()
+# 結果通知
 lab2 = Label(win, text="目標:")   #第二個加入視窗 win 的物件
 lab2.pack()
-#加入 網址文字框輸入
-urlEntry = Entry(win, font=('Arial',15), fg='blue')
-urlEntry.pack()
 #第三個物件 : 按鈕
 okBtn = Button(win ,font=('Arial',15), text="下載" , fg='blue', command=URLGetClick)
 okBtn.pack()
+clearBtn = Button(win ,font=('Arial',15), text="x" , fg='blue', command=clearURLEntry)
+clearBtn.pack()
+
 #win.resizable(0,0)
 
 
